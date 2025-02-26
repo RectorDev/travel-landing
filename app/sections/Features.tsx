7a9440d933b08b12385a054c1bc40250750963eb
RectorDev <Rector.dev@gmail.com>
2025-02-26 15:44:36 +0330

    commit

--- app/sections/Features.tsx
+++ app/sections/Features.tsx
@@ -1,6 +1,37 @@
+import { FEATURES } from "@/constants";
+import Image from "next/image";
 
 export default function Features() {
   return (
-    <div>Features</div>
-  )
+    <section className="flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image src={"/phone.png"} alt="phone" width={440} height={100} className="feature-phone" />
+        </div>
+        <div className="z-20 flex w-full flex-col lg:w-[60%] ">
+          <div className="relative">
+            <Image src={"/camp.svg"} className="absolute right-[-5px] top-[-49px]" alt="camp" width={50} height={50} />
+            <h2 dir="rtl" className="bold-40 lg:bold-64">قابلیت های ما</h2>
+          </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20 " dir="rtl">
+            {FEATURES.map((item) => (
+              <FeatureItem title={item.title} key={item.icon} icon={item.icon} description={item.description} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function FeatureItem({ title, icon, description }: { icon: string; description: string; title: string }) {
+  return (
+    <li className="flex flex-1 w-full flex-col items-start ">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold32 mt-5 ">{title}</h2>
+      <p className="regular-16 mt-5 bg-white/50 text-secondary/70 lg:mt-[30px] lg:bg-none">{description}</p>
+    </li>
+  );
 }
--- app/sections/Guide.tsx
+++ app/sections/Guide.tsx
@@ -1,6 +1,40 @@
+import Image from "next/image";
 
 export default function Guide() {
   return (
-    <div>guide</div>
-  )
+    <section className="flexCenter flex-col" dir="rtl">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src={"/camp.svg"} alt="camp" width={50} height={50} />
+        <p className=" regular-18 -mt-1 mb-3 text-green-50">ما برای شما هستیم</p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-14  lg:bold-64 xl:max-w-[560px]">راهنمای شما برای مسیری آسان</h2>
+          <p className="regular-16 text-secondary/70 xl:max-w-[520px]">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای
+            شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در
+          </p>
+        </div>
+      </div>
+      <div className="flexCenter max-container relative w-full ">
+        <Image src={"/boat.png"} alt="boat" width={1440} height={580} className="w-full object-cover object-center 2xl:rounded-5xl" />
+        <div className="absolute flex bg-primary py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20 w-64">
+          <Image src={"/meter.svg"} alt="meter" width={16} height={158} className="h-full w-auto" />
+          <div className="flexBetween flex-col  w-full gap-10 ">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween  w-full">
+                <p className="regular-16 text-secondary/70">مقصد</p>
+                <p className="bold-16 text-green-50">48 دقیقه</p>
+              </div>
+              <p className="bold-20 mt-2">دریاچه </p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-secondary/70">مبدا</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">نیشابور</h4>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
